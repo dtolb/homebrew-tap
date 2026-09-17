55c570275096c132ed1368d4932fb869d670f210
dropper-ci <ci@dropp.sh>
2026-09-17 21:45:18 +0000

    dropper 0.18.1

--- Casks/dropper.rb
+++ Casks/dropper.rb
@@ -1,7 +1,7 @@
 cask "dropper" do
   arch arm: "arm64", intel: "x64"
-  version "0.18.0"
-  sha256 arm: "c38a4dbc07940ffc8e9b04560822f8d8040d10b91e564f5fb4bb17f3ffffaf3e", intel: "b3dd51bf26e19b7ecf0c243ba6f101435460775481ae78e376a4f65c696511a4"
+  version "0.18.1"
+  sha256 arm: "506bdac4fb98d89851d3e3aed8652ca89d48fddec19b6c456fe75c59ae9fb670", intel: "52377f01b512fd1ec2e67411e19108598f3ec2adc09a037f65eec4da8757bf8a"
   url "https://dropp.sh/cli/v#{version}/dropper-darwin-#{arch}.tar.gz"
   name "Dropper CLI"
   desc "Drop a static page, get a URL — CLI for dropp.sh"
